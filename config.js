bfabc5e1231af30abe783f5560da7917cf8c3cec
longthanhhaa <94179847+longthanhhaa@users.noreply.github.com>
2021-11-12 09:46:01 -0500

    Create config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "Tri ân ngày Nhà giáo Việt Nam",
-    introTitle: 'Thầy cô HVTC',
+    introTitle: 'Thầy cô Học Viện Tài Chính',
     introDesc: `Thầy cô là người truyền cảm hứng, mang đến cho chúng em những kiến thức bổ ích. Nhân dịp ngày 20/11, thay lời muốn nói, em xin gửi tới thầy cô những lời chúc tốt đẹp nhất.`,
     btnIntro: 'Thiệp chúc mừng',
     title: 'Chúc mừng ngày nhà Giáo Việt Nam 20/11',
@@ -11,7 +11,7 @@ const CONFIG = {
     btnReply: 'Gửi cho ban văn phòng',
     reply: 'cảm ơn tất cả các em',
     mess: 'em cảm ơn thầy cô',
-    messDesc: ':)))',
+    messDesc: '',
     btnAccept: 'gửi',
     messLink: 'https://fb.com/zukahai/Confess-20/11' //https://m.me/lthaa.211
 }
